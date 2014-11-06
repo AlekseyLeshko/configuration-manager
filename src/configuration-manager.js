3fb132cc1709ca1423435d163803b64592c895c2
alekseyleshko <alekseyleshko@gmail.com>
2014-11-06 19:43:31 +0300

    Update

--- src/configuration-manager.js
+++ src/configuration-manager.js
@@ -1,4 +1,26 @@
-var argv = require('optimist').argv;
+// process.argv.forEach(function (val, index, array) {
+  // console.log(index + ': ' + val);
+// });
 
-console.log('Hello ' + (argv.name || 'World') + '!');
+var parser = require("nomnom");
+parser.command('browser')
+   .callback(function(opts) {
+      if (!opts.type) {
+        return;
+      }
+      console.log(opts.type);
+      var funName = opts.type + '()';
+      eval(funName);
+   })
+   .help("run browser tests");
+parser.parse();
+
+function major() {
+  console.log('function major');
+}
+
+function minor() {
+  console.log('function minor');
+}
+
 console.log('It is npm module: \'Configuration-manager!\'');
