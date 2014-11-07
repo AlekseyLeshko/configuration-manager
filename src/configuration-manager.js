e4fb188e996fc1af64039f8d0d7fe62183d3cf9d
alekseyleshko <alekseyleshko@gmail.com>
2014-11-07 13:38:34 +0300

    Update configuration-manager

--- src/configuration-manager.js
+++ src/configuration-manager.js
@@ -1,26 +1,19 @@
-// process.argv.forEach(function (val, index, array) {
-  // console.log(index + ': ' + val);
-// });
+(function() {
+  'use strict';
 
-var parser = require("nomnom");
-parser.command('browser')
-   .callback(function(opts) {
-      if (!opts.type) {
-        return;
-      }
-      console.log(opts.type);
-      var funName = opts.type + '()';
-      eval(funName);
-   })
-   .help("run browser tests");
-parser.parse();
+  var ConfigurationManager = function() {
+    this.name = 'ConfigurationManager';
+  };
 
-function major() {
-  console.log('function major');
-}
+  ConfigurationManager.prototype = {
+    major: function() {
+      console.log('function major');
+    },
 
-function minor() {
-  console.log('function minor');
-}
+    minor: function() {
+      console.log('function minor');
+    }
+  };
 
-console.log('It is npm module: \'Configuration-manager!\'');
+  module.exports = ConfigurationManager;
+})();
