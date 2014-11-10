eb665a0a7bc3e825a5186110b6eac830144d38dd
alekseyleshko <alekseyleshko@gmail.com>
2014-11-10 18:16:12 +0300

    Fix

--- spec/config.spec.js
+++ spec/config.spec.js
@@ -7,16 +7,12 @@ var sh = require('shelljs');
 describe('Config', function() {
   var config = new Config();
 
-  beforeEach(function() {
-    expect(fs.existsSync(config.config.dirName)).toBeFalsy();
-  });
-
   afterEach(function() {
     sh.rm('-rf', config.config.dirName);
     expect(fs.existsSync(config.config.dirName)).toBeFalsy();
   });
 
-  it('should init', function() {
+  it('should init make default config', function() {
     expect(config.config.dirName).toEqual('config/');
     expect(config.config.fileName).toEqual('config.json');
   });
@@ -27,8 +23,12 @@ describe('Config', function() {
     expect(path).toEqual(expected);
   });
 
-  it('should create', function() {
+  it('should create folder with config json', function() {
+    expect(fs.existsSync(config.config.dirName)).toBeFalsy();
+
     config.create();
+
     expect(fs.existsSync(config.config.dirName)).toBeTruthy();
+    expect(fs.existsSync(config.getPath())).toBeTruthy();
   });
 });
--- spec/configurationManager.spec.js
+++ spec/configurationManager.spec.js
@@ -1,17 +1,30 @@
 'use strict';
+
 var ConfigurationManager = require('../src/configuration-manager');
+var fs = require('fs');
+var sh = require('shelljs');
 
 describe('Configuration manager', function() {
   var cManager = new ConfigurationManager();
+  var dirName = 'config/';
+  var path = 'config/config.json';
 
   beforeEach(function() {
     spyOn(cManager.configFile, 'isExistOrCreate').and.callThrough();
   });
 
+  afterEach(function() {
+    sh.rm('-rf', dirName);
+    expect(fs.existsSync(dirName)).toBeFalsy();
+  });
+
   it('should run isExistOrCreate', function() {
+    expect(fs.existsSync(dirName)).toBeFalsy();
+
     cManager.init();
 
     expect(cManager.configFile.isExistOrCreate).toHaveBeenCalled();
+    expect(fs.existsSync(path)).toBeTruthy();
   });
 
   it('should ', function() {
