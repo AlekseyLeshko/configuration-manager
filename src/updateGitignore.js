92df34cf5b5b77bf769b927be6a459f20a601e53
alekseyleshko <alekseyleshko@gmail.com>
2014-11-11 19:29:20 +0300

    Fix

--- src/updateGitignore.js
+++ src/updateGitignore.js
@@ -8,21 +8,14 @@
 
     var gitignore = fs.readFileSync(gitignorePath, 'utf8');
     var list = gitignore.split('\n');
-    console.log(list);
 
     var path = 'config/config.json';
     if (list.indexOf(path) == -1) {
       list.push(path);
     }
     list = list.join('\n');
-    console.log(list);
-    fs.writeFile(gitignorePath, list, function(err) {
-      if(err) {
-          console.log(err);
-      } else {
-          console.log("The file was saved!");
-      }
-    });
+    fs.writeFile(gitignorePath, list);
+    console.log('.gitignore file updated');
   }
 
   module.exports = updateGitignore;
