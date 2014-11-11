d83c30409360ffb5c381d6de251b68f73f139145
alekseyleshko <alekseyleshko@gmail.com>
2014-11-11 19:29:57 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -18,35 +18,29 @@
     },
 
     isExistOrCreate: function() {
-      this.isExist()
-        .then(function(value) {
-          this.create();
-          console.log('create default config file');
-        }, function() {
-          console.log('Config file is exist in this module');
-        });
+      if (this.isExist()) {
+        console.log('Config file is exist in this module');
+        return;
+      }
+
+      this.create();
+      console.log('create default config file');
     },
 
     isExist: function() {
-      var deferred = Q.defer();
-      if (!fs.existsSync(this.config.dirName)) {
-        deferred.resolve(false);
-      }
       if (!fs.existsSync(this.getPath())) {
-        deferred.resolve(false);
+        return false;
       }
-      deferred.resolve(true);
-
-      return deferred.promise;
+      return true;
     },
 
     create: function() {
-      var deferred = Q.defer();
-      fs.mkdir(this.config.dirName, deferred.makeNodeResolver());
+      if (!fs.existsSync(this.config.dirName)) {
+        fs.mkdir(this.config.dirName);
+      }
 
       var json = JSON.stringify(defaultConfig, null, 2);
-      fs.writeFile(this.getPath(), json, deferred.makeNodeResolver());
-      return deferred.promise;
+      fs.writeFile(this.getPath(), json);
     },
 
     getPath: function() {
