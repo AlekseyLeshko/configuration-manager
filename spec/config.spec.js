2ac081c2c51b5b9b06e10c0ef5de17ab59200929
alekseyleshko <alekseyleshko@gmail.com>
2014-11-11 16:14:21 +0300

    Update tests

--- spec/config.spec.js
+++ spec/config.spec.js
@@ -2,16 +2,23 @@
 
 var Config = require('../src/config');
 var fs = require('fs');
-var mkdirp = require('mkdirp');
 var sh = require('shelljs');
+var Q = require('q');
 
 describe('Config', function() {
   var config = new Config();
+  var defaultConfig = {
+    a: 1,
+    b: 2,
+    c: 3
+  };
 
-  afterEach(function() {
+  afterEach(function(done) {
     sh.rm('-rf', config.config.dirName);
-    fs.exists(config.config.dirName, function (exists) {
+    Q.fcall(function () {
+      var exists = fs.existsSync(config.config.dirName);
       expect(exists).toBeFalsy();
+      done();
     });
   });
 
@@ -26,49 +33,123 @@ describe('Config', function() {
     expect(path).toEqual(expected);
   });
 
-  it('should create folder with config json', function() {
-    fs.exists(config.config.dirName, function (exists) {
-      expect(exists).toBeFalsy();
-    });
-
-    config.create();
-
-    fs.exists(config.config.dirName, function (exists) {
-      expect(exists).toBeTruthy();
-    });
-    fs.exists(config.getPath(), function (exists) {
-      expect(exists).toBeTruthy();
-    });
+  it('should be established folder with config json', function(done) {
+    Q.fcall(function () {
+        var exists = fs.existsSync(config.config.dirName);
+        expect(exists).toBeFalsy();
+      })
+      .then(function() {
+        config.create()
+          .then(function() {
+            expect(true).toBeTruthy();
+          }, function() {
+            expect(true).toBeFalsy();
+          });
+      })
+      .then(function() {
+        var exists = fs.existsSync(config.config.dirName);
+        expect(exists).toBeTruthy();
+      })
+      .then(function() {
+        var exists = fs.existsSync(config.getPath());
+        expect(exists).toBeTruthy();
+        done();
+      });
   });
 
-  it('should isExist without folder return false', function() {
-    fs.exists(config.config.dirName, function (exists) {
-      expect(exists).toBeFalsy();
-    });
-    var res = config.isExist();
-
-    expect(res).toBeFalsy();
+  it('should create config json when folder exists', function(done) {
+    Q.fcall(function () {
+        fs.mkdir(config.config.dirName);
+      })
+      .then(function() {
+        var exists = fs.existsSync(config.config.dirName);
+        expect(exists).toBeTruthy();
+      })
+      .then(function() {
+        var exists = fs.existsSync(config.config.fileName);
+        expect(exists).toBeFalsy();
+      })
+      .then(function() {
+        config.create()
+          .then(function() {
+            expect(true).toBeTruthy();
+          }, function() {
+            expect(true).toBeFalsy();
+          });
+      })
+      .then(function() {
+        var exists = fs.existsSync(config.config.dirName);
+        expect(exists).toBeTruthy();
+      })
+      .then(function() {
+        var exists = fs.existsSync(config.getPath());
+        expect(exists).toBeTruthy();
+        done();
+      });
   });
 
-  it('should isExist without json file return false', function() {
-    mkdirp(config.config.dirName);
-    fs.exists(config.config.fileName, function (exists) {
-      expect(exists).toBeFalsy();
-    });
-
-    var res = config.isExist();
-
-    expect(res).toBeFalsy();
+  it('should isExist without folder return false', function(done) {
+    Q.fcall(function() {
+        var exists = fs.existsSync(config.config.dirName);
+        expect(exists).toBeFalsy();
+      })
+      .then(function() {
+        return config.isExist()
+          .then(function(value) {
+            expect(value).toBeFalsy();
+          }, function() {
+            expect(true).toBeFalsy();
+          });
+      })
+      .done(function() {
+        done();
+      });
   });
 
-  it('should isExist return true', function() {
-    config.create();
-    fs.exists(config.config.fileName, function (exists) {
-      expect(exists).toBeFalsy();
-    });
+  it('should isExist without json file return false', function(done) {
+    Q.fcall(function () {
+        fs.mkdirSync(config.config.dirName);
+      })
+      .then(function() {
+        var exists = fs.existsSync(config.config.fileName);
+        expect(exists).toBeFalsy();
+      })
+      .then(function() {
+        return config.isExist()
+          .then(function(value) {
+            expect(value).toBeFalsy();
+          }, function() {
+            expect(true).toBeFalsy();
+          });
+      })
+      .done(function() {
+        done();
+      });
+  });
 
-    var res = config.isExist();
+  it('should isExist return true', function(done) {
+    Q.fcall(function () {
+      })
+      .then(function() {
+        fs.mkdir(config.config.dirName);
 
-    expect(res).toBeTruthy();
+        var json = JSON.stringify(defaultConfig, null, 2);
+        fs.writeFile(config.getPath(), json);
+      })
+      .then(function() {
+        var exists = fs.existsSync(config.config.fileName);
+        expect(exists).toBeFalsy();
+      })
+      .then(function() {
+        return config.isExist()
+          .then(function(value) {
+            expect(value).toBeTruthy();
+          }, function() {
+            expect(true).toBeFalsy();
+          });
+      })
+      .done(function() {
+        done();
+      });
   });
 });
