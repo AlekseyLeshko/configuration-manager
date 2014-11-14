a802aad28ba04dc34ba554bd951a10c0aea6bace
alekseyleshko <alekseyleshko@gmail.com>
2014-11-14 19:06:32 +0300

    Fix test

--- spec/version.spec.js
+++ spec/version.spec.js
@@ -8,53 +8,22 @@ describe('version', function() {
   var version = new Version();
 
   beforeEach(function() {
-    cManager.setVersion = function(newVersion) {
-      version = newVersion;
-    };
-
-    cManager.getConfig = function() {
+    version.configFile.get = function() {
       var config = {
         version: '0.0.0'
       };
       return config;
     };
-    spyOn(cManager, 'getConfig').and.callThrough();
-    spyOn(cManager, 'setVersion').and.callThrough();
+
+    spyOn(version.configFile, 'get').and.callThrough();
   });
 
-  it('should getVersion without type', function() {
-    var expected = '0.0.1'
+  it('should get', function() {
+    var expected = '0.0.0'
 
-    cManager.incVersion();
+    var res = version.get();
 
-    expect(cManager.getConfig).toHaveBeenCalled();
-    expect(version).toEqual(expected);
-  });
-
-  it('should incPatch', function() {
-    var expected = '0.0.1'
-
-    cManager.incPatch();
-
-    expect(cManager.getConfig).toHaveBeenCalled();
-    expect(version).toEqual(expected);
-  });
-
-  it('should incMminor', function() {
-    var expected = '0.1.0'
-
-    cManager.incMminor();
-
-    expect(cManager.getConfig).toHaveBeenCalled();
-    expect(version).toEqual(expected);
-  });
-
-  it('should incMajor', function() {
-    var expected = '1.0.0'
-
-    cManager.incMajor();
-
-    expect(cManager.getConfig).toHaveBeenCalled();
-    expect(version).toEqual(expected);
+    expect(version.configFile.get).toHaveBeenCalled();
+    expect(res).toEqual(expected);
   });
 });
