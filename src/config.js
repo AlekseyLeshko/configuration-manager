2800d33b97f89873e09c839c2dfc466cf9424f1a
alekseyleshko <alekseyleshko@gmail.com>
2014-11-18 16:05:29 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,19 +2,34 @@
   'use strict';
 
   var fs = require('fs');
+  var memFs = require('mem-fs');
+  var editor = require('mem-fs-editor');
   var defaultConfig = require('./defaultConfig');
-  var Q = require('q');
 
   var Config = function() {
     this.init();
+    this.load();
   };
 
   Config.prototype = {
     init: function() {
+      var store = memFs.create();
+      this.fs = editor.create(store);
+
       this.config = {
         dirName: 'config/',
         fileName: 'config-manager.json'
       };
+    },
+
+    load: function() {
+      this.isExistOrCreate();
+      this.data = this.fs.readJSON(this.getPath());
+    },
+
+    save: function() {
+      var json = JSON.stringify(obj, null, 2);
+      fs.writeFile(this.getPath(), json);
     },
 
     isExistOrCreate: function() {
@@ -35,28 +50,24 @@
     },
 
     create: function() {
+      this.craeteDir();
+
+      // console.log(defaultConfig);
+      var json = JSON.stringify(defaultConfig, null, 2);
+      // console.log(json);
+      fs.writeFile(this.getPath(), json);
+      this.fs.write(this.getPath(), json);
+    },
+
+    craeteDir: function() {
       if (!fs.existsSync(this.config.dirName)) {
         fs.mkdir(this.config.dirName);
       }
-
-      var json = JSON.stringify(defaultConfig, null, 2);
-      fs.writeFile(this.getPath(), json);
     },
 
     getPath: function() {
       var path = this.config.dirName + this.config.fileName;
       return path;
-    },
-
-    get: function() {
-      var data = fs.readFileSync(this.getPath(), 'utf8');
-      var obj = JSON.parse(data);
-      return obj;
-    },
-
-    set: function(obj) {
-      var json = JSON.stringify(obj, null, 2);
-      fs.writeFile(this.getPath(), json);
     }
   };
 
