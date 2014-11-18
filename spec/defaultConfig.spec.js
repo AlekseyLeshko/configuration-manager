263deb56c809e9ee691527791de97a19824fdb7b
alekseyleshko <alekseyleshko@gmail.com>
2014-11-18 14:49:25 +0300

    Remove spec folder

--- spec/defaultConfig.spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var DefaultConfig = require('../src/defaultConfig');
-
-describe('Default config', function() {
-  it('should exist', function() {
-    expect(DefaultConfig).toBeDefined();
-    expect(DefaultConfig).not.toBeNull();
-  });
-});
